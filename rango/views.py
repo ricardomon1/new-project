4660728b73310d77efe557b93148929f37105b59
Ricardo Mon <ricardo.mon1@utp.ac.pa>
2022-04-29 10:54:56 -0500

    proyecto
    ...

---
+++ rango/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    context_dict= {"boldmesage":Crunchy, creamy, cookie, candy, cupcake}
+    #return HttpResponse("Rango say here")
+    return render(request, ' rango/index.html,'context=context_dict)
+
+def about(request):
+    return HttpResponse("rango says here about page")
